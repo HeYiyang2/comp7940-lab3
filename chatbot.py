29129ff1e2ad9b822ac636458b6eab2c70b082f8
HeYiyang2 <24435988@life.hkbu.edu.hk>
2025-02-26 14:59:43 +0800

    First commit

---
+++ chatbot.py
@@ -0,0 +1,28 @@
+from telegram import Update
+from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters,
+CallbackContext)
+import configparser
+import logging
+
+def main():
+    # Load your token and create an Updater for your Bot
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    updater = Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']), use_context=True)
+    dispatcher = updater.dispatcher
+    # You can set this logging module, so you will know when
+    # and why things do not work as expected Meanwhile, update your config.ini as:
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
+    # register a dispatcher to handle message: here we register an echo dispatcher
+    echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+    # To start the bot:
+    updater.start_polling()
+    updater.idle()
+def echo(update, context):
+    reply_message = update.message.text.upper()
+    logging.info("Update: " + str(update))
+    logging.info("context: " + str(context))
+    context.bot.send_message(chat_id=update.effective_chat.id, text= reply_message)
+
+if __name__ == '__main__':
+    main()
